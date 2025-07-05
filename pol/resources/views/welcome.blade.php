540d4b95f8924c44e2be86c022f4c15805e27786
johnpol26 <johnpolbmontalvo@gmail.com>
2025-07-05 12:10:57 +0800

    Modified welcome css file

--- pol/resources/views/welcome.blade.php
+++ pol/resources/views/welcome.blade.php
@@ -21,6 +21,9 @@
             <h1 class="hi">Hi,</h1><br>
             <h1 class="myname"> My name is John Pol Montalvo</h1><br><br>
             <h1 class="webdev">Web Developer</h1>
+            <p class="intro-description">
+                A web developer who builds clean, responsive websites with purpose.
+            </p>
         </div>
         <div class="intro-image">
             <img src="{{ asset('css/pol.jpg') }}" alt="John Pol Montalvo">
